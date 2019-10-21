6bda49e5d137ea129e81a3cf83ea1faabc4ef249
sfedynya <sfedynya@cisco.com>
2019-10-21 13:00:06 +0300

    update 21.10

--- log-and-func-prog/lab1/task18.py
+++ log-and-func-prog/lab1/task18.py
@@ -1,19 +1,63 @@
+import itertools
+import string
+from functools import reduce
 import re
 
-passwords = open('data/passwords.txt', 'r').readline().split(',')
-print('Source: {}'.format(passwords))
-res = []
-for password in passwords:
-    if len(password) < 6 or len (password) > 12:
-        continue
-    if not re.search('[a-z]', password):
-        continue
-    if not re.search("[0-9]", password):
-        continue
-    if not re.search("[A-Z]", password):
-        continue
-    if not re.search("[$#@]", password):
-        continue
-    res.append(password)
-print (res)
+def check(passwords, *functions):
+    funcs = list(functions)
+    result  = reduce(lambda res, f: f(res), funcs, passwords)
+    return result
 
+def read_data():
+    all_passwords = open('data/passwords.txt', 'r').read()
+    passwords = [x for x in all_passwords.split(",")]
+    return passwords
+    
+def password_has_digits( current_string, index = 0):
+    if(index == len(current_string)):
+        return False
+
+    if current_string[index] in (str(x) for x in range(0,10)):
+        return True
+    else:
+        return password_has_digits(current_string, index+1)
+
+def password_has_lowercase_letters(current_string, index = 0):
+    if index == len(current_string):
+        return False
+    if current_string[index] in [x for x in string.ascii_lowercase]:
+        return True
+    else:
+        return password_has_lowercase_letters(current_string, index+1)
+
+def length_check(passwords):
+    result = list(filter(lambda x: len(x)>=6 and len(x)<=12, passwords))
+    return result
+def lowercase_check(passwords):
+    result = list(filter (password_has_lowercase_letters, passwords))
+    return result
+def uppercase_check(passwords):
+    result = list(filter(lambda x: re.search("[A-Z]", x), passwords))
+    return result
+def digits_check(passwords):
+    result = list(filter(password_has_digits, passwords))
+    return result
+
+def special_symbol_check(func, symb):
+    def inner(passwords):
+        result = func(passwords, symb)
+        return result
+    return inner
+
+
+def symb_check(passwords, symbols=""):
+    result = list(filter(lambda x: (len(set(x).intersection(set(symbols))) != 0), passwords))
+    return result
+
+def main():
+    passwords = read_data()
+    #valid password example: ABcd12@dnj, not valid - abc1
+    inner_symbol_check = special_symbol_check(symb_check, '$#@')
+    res = check(passwords, lowercase_check, uppercase_check, digits_check, length_check, inner_symbol_check)
+    print(res)
+main()
--- log-and-func-prog/lab1/task18.sh
+++ log-and-func-prog/lab1/task18.sh
@@ -1,35 +1,56 @@
 #!/bin/bash
 set -x
-passwords=`cat data/passwords.txt`
-res=""
-echo "$passwords"
-input=`echo "$passwords" | tr ',' '\n'`
-while IFS= read -r password
-do
-    len=`echo "$password" | wc -m`
-    if [ "$len" -le 6 ] || [ "$len" -ge 12 ];then
-        continue
-    fi
-    echo "$password" | grep -q '[a-z]'
-    if [ $? -ne 0 ];then
-        continue
-    fi
-    echo "$password" | grep -q '[A-Z]'
-    if [ $? -ne 0 ];then
-        continue
-    fi
+
+check_digits() {
+    password=$1
     echo "$password" | grep -q '[0-9]'
-    if [ $? -ne 0 ];then
-        continue
+    return $?
+}
+check_low() {
+    password=$1
+    echo "$password" | grep -q '[a-z]'
+    return $?
+}
+check_high() {
+    password=$1
+    echo "$password" | grep -q '[A-Z]'
+    return $?
+}
+check_special() {
+    password=$1
+    echo "$password" | grep -q '[#$@]'
+    return $?
+}
+check() {
+    curr_string=$1
+    if [ "$curr_string" == '' ];then
+        return 0
+    elif [[ "$curr_string" = ,* ]];then
+        curr_string=`echo "$curr_string" | sed 's/,//g'`
+        check "$curr_string"
+    else
+        password=`echo "$curr_string" | cut -d, -f1`
+        check_digits "$password" && \
+        check_high "$password" && \
+        check_low "$password" && \
+        check_special "$password"
+        if [ $? -eq 0 ];then
+            if [ "$res" == '' ];then
+                res="$password"
+            else
+                res="$res, $password"
+            fi
+        fi
+        curr_string=`echo "$curr_string" | "s/$password//g"`
+        check "$curr_string"
     fi
-    echo "$password" | grep -q '[$#@]'
-    if [ $? -ne 0 ];then
-        continue
-    fi
-    if [ "$res" == "" ];then
-            res="$password"
-        else
-            res="$res, $password"
-    fi
-done <<< "$input"
-echo "$res"
+}
+
+main() {
+    passwords=`cat data/passwords.txt`
+    res=''
+    check "$passwords"
+    echo "$res"
+}
+
+main
