0862ce9465b9388fc5d045b314393482a4170f30
sfedynya <sfedynya@cisco.com>
2019-12-02 18:38:40 +0200

    Python term add multithr

--- python/term-proj/main.py
+++ python/term-proj/main.py
@@ -1,5 +1,7 @@
-import os, sys
+import os
+import sys
 import subprocess
+import threading
 
 child = './child.py'
 pipes = []
@@ -11,12 +13,24 @@ command = [sys.executable, child, 'random', '1', '1', '100']
 pipe = subprocess.Popen(command, stdout=subprocess.PIPE)
 pipes.append(pipe)
 
-buffer = 'temp'
 
-while buffer:
-    for pipe in pipes:
+def create_and_poll(process, *args):    
+    command = [sys.executable, child, process].extend(args)
+    pipe = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+    pipes.append(pipe)
+    
+    buffer = 'temp'
+
+    while buffer:
         buffer = pipe.stdout.readline()
         print(buffer.decode('utf8'), end='')
-  
+        
+threads = []
 
+for pipe in pipes:
+    work_thread = threading.Thread(target=create_and_poll, args=(pipe))
+    threads.append(work_thread)
+    work_thread.daemon = True
 
+for thread in threads:
+    thread.start()
